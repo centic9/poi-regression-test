765b7090a930079cd97a73296a145d408cc3538a
Dominik Stadler <dominik.stadler@gmx.at>
2023-11-30 19:26:26 +0100

    Sync some changes, add 5.2.5-RC1, switch to 5.2.6-SNAPSHOT

--- src/main/java/org/dstadler/commoncrawl/jpa/POIStatus.java
+++ src/main/java/org/dstadler/commoncrawl/jpa/POIStatus.java
@@ -57,7 +57,8 @@ import com.google.common.base.Preconditions;
 		"exceptionStacktrace510RC2","exceptionText510RC2","poi510RC2",
 		"exceptionStacktrace520RC1","exceptionText520RC1","poi520RC1",
 		"exceptionStacktrace523RC1","exceptionText523RC1","poi523RC1",
-		"exceptionStacktrace524RC1","exceptionText524RC1","poi524RC1" })
+		"exceptionStacktrace524RC1","exceptionText524RC1","poi524RC1",
+		"exceptionStacktrace525RC1","exceptionText525RC1","poi525RC1" })
 @Entity
 public class POIStatus extends Base {
 	@JsonProperty
@@ -605,6 +606,24 @@ public class POIStatus extends Base {
 	@Basic
 	private long duration524RC1;
 
+	@JsonProperty
+	@Basic
+	private FileStatus poi525RC1;
+
+	@JsonProperty
+	@Basic
+	@Column(length = FileURL.URL_MAX_LENGTH)
+	private String exceptionText525RC1;
+
+	@JsonProperty
+	@Basic
+	@Column(length = FileURL.URL_MAX_LENGTH)
+	private String exceptionStacktrace525RC1;
+
+	@JsonProperty
+	@Basic
+	private long duration525RC1;
+
 	public POIStatus() {
 		super();
 	}
@@ -896,6 +915,14 @@ public class POIStatus extends Base {
 		this.poi524RC1 = poi524RC1;
 	}
 
+	public FileStatus getPoi525RC1() {
+		return poi525RC1;
+	}
+
+	public void setPoi525RC1(FileStatus poi525RC1) {
+		this.poi525RC1 = poi525RC1;
+	}
+
 	public void setByVersion(String version, ResultItem item) {
 		final FileStatus status;
 		if(item.isTimeout() ||
@@ -1072,6 +1099,11 @@ public class POIStatus extends Base {
 			exceptionText524RC1 = StringUtils.abbreviate(item.getExceptionText(), FileURL.URL_MAX_LENGTH);
 			exceptionStacktrace524RC1 = StringUtils.abbreviate(item.getExceptionStacktrace(), FileURL.URL_MAX_LENGTH);
 			duration524RC1 = item.getDuration();
+		} else if (version.contains("5.2.5-RC1")) {
+			this.poi525RC1 = status;
+			exceptionText525RC1 = StringUtils.abbreviate(item.getExceptionText(), FileURL.URL_MAX_LENGTH);
+			exceptionStacktrace525RC1 = StringUtils.abbreviate(item.getExceptionStacktrace(), FileURL.URL_MAX_LENGTH);
+			duration525RC1 = item.getDuration();
 		} else {
 			throw new IllegalStateException("Unknown version found: " + version);
 		}
